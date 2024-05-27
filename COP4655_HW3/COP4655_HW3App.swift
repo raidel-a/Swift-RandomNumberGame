f03a3e066b97f4a3779b438392865ea42279e381
raidel-a <raihven@protonmail.com>
2024-05-26 23:03:53 -0400

    Initial Commit

---
+++ COP4655_HW3/COP4655_HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  COP4655_HW3App.swift
+//  COP4655_HW3
+//
+//  Created by Raidel Almeida on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct COP4655_HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COP4655_HW3UITests/COP4655_HW3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  COP4655_HW3UITestsLaunchTests.swift
+//  COP4655_HW3UITests
+//
+//  Created by Raidel Almeida on 5/26/24.
+//
+
+import XCTest
+
+final class COP4655_HW3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
